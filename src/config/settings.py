13091c03d7293730f9a19d7f62648e9873585b06
Luis Rodolfo Rodriguez Betancourt <lrbetancourt@uuci.cu>
2025-07-22 23:09:39 -0400

    Fix(db): ssl mode require

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,22 +1,18 @@
-#from dotenv import load_nv
 import os
 
-#load_dotenv()
-
 class Settings:
-    SECRET_KEY: str = os.environ["SECRET_KEY"]
-
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "default_secret")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
-    DB_USER: str = os.environ["DB_USER"]
-    DB_PASSWORD: str = os.environ["DB_PASSWORD"]
-    DB_HOST: str = os.environ["DB_HOST"]
-    DB_PORT: str = os.environ["DB_PORT"]
-    DB_NAME: str = os.environ["DB_NAME"]
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_NAME: str = os.getenv("DB_NAME", "postgres")
 
     DATABASE_URL: str = (
-        f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
     )
 
 settings = Settings()
